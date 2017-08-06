14d751a66b187f607a0b6f5ae9aedf2555a067ce
Katie Allyn <kaallyn@gmail.com>
2017-08-06 11:16:07 -0500

    adding content

---
+++ README.md
@@ -0,0 +1,3 @@
+# HTML & CSS Boilerplate
+
+A great place to get started. This boilerplate uses HTML, CSS, and Javascript. It is a great starting point for **any** project!
---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+
+window.onload = function() {
+  //alert('hey hey hey');
+//   statusReport('hey HEY HEY!!!!!');
+
+//   var username = prompt('What is your name?');
+//   var mission = prompt('What is your mission?');
+
+//   var contentContainer = document.getElementById('content');
+
+//   contentContainer.innerHTML = username + ' is on a mission to ' + mission;
+
+// }
+
+// function statusReport(message) {
+//   console.log(message);
+//   return message;
+}
